2d05a484c072f63ca6ec6caf8f93857dd96093a1
Mohammed Elsammak <elsamak_1@yahoo.com>
2017-08-16 14:27:54 +0200

    First commit.

---
+++ ModesoRubberBandScrollView/ModesoRubberBandScrollView/ModesoRubberBandScrollView.h
@@ -0,0 +1,19 @@
+//
+//  ModesoRubberBandScrollView.h
+//  ModesoRubberBandScrollView
+//
+//  Created by Mohammed Elsammak on 8/16/17.
+//  Copyright © 2017 Modeso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ModesoRubberBandScrollView.
+FOUNDATION_EXPORT double ModesoRubberBandScrollViewVersionNumber;
+
+//! Project version string for ModesoRubberBandScrollView.
+FOUNDATION_EXPORT const unsigned char ModesoRubberBandScrollViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ModesoRubberBandScrollView/PublicHeader.h>
+
+
